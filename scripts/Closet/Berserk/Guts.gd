6dea7eaf724e4ca9cee45b917db3101c982e9bd4
patrikTrebichavsky <88612821+patrikTrebichavsky@users.noreply.github.com>
2025-05-26 16:19:51 +0200

    Guts HitBox
    Ehej

--- scripts/Closet/Berserk/Guts.gd
+++ scripts/Closet/Berserk/Guts.gd
@@ -1,17 +1,22 @@
 extends RigidBody2D
 
-@export var speed = 400
+@export var speed = 600
 var current_velocity = 0
-@export var enemy_position = Vector2(1920,1080)
+var enemy = null
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
 var target_position : Vector2
+@export var push_back_multiplier = 10
 
 var attack = 0
 var attacking = false
 
+var damage = 3
+
 @export var undertale_mode : bool
 
 var  temp
+
+var  bodies
 
 func _ready():
 	
@@ -23,16 +28,33 @@ func _physics_process(_delta):
 	if attacking:
 		return
 	
-	var temp = (position-enemy_position).abs()
+	if enemy != null:
+		if enemy.hp <= 0:
+			enemy = null
 	
-	if temp.x < 100 && temp.y <100:
-		self.look_at(enemy_position)
+	if enemy == null:
+		bodies = $DetectionArea2D.get_overlapping_bodies()
+		for body in bodies:
+			if "Wall" in body.name or body.name == "Guts":
+				continue
+			
+			if enemy == null:
+				enemy = body
+			elif (position - body.position).abs().length() < (position - enemy.position).abs().length():
+				enemy = body 
+	if enemy == null:
+		return
+	
+	var temp = (position-enemy.position).abs()
+	
+	if temp.x < 100 && temp.y < 100:
+		self.look_at(enemy.position)
 		attacking = true
 		linear_velocity = Vector2.ZERO
 		match  attack:
 			0:
 				attack = 1
-				$AnimationPlayer.play("SwingLeft")
+				$AnimationPlayer.play("Swing")
 			1:
 				attack = 2
 				$AnimationPlayer.play("Spin")
@@ -41,12 +63,12 @@ func _physics_process(_delta):
 				$AnimationPlayer.play("Overhead")
 	else:
 	
-		nav.target_position = enemy_position
+		nav.target_position = enemy.position
 		target_position = (nav.get_next_path_position()-global_position).normalized()
 		current_velocity = target_position * speed
 		nav.set_velocity(current_velocity) 	
 		self.look_at(nav.get_next_path_position())
-
+	
 	
 func move(velocity: Vector2):
 
@@ -55,16 +77,35 @@ func move(velocity: Vector2):
 
 	linear_velocity = velocity
 	
-
-	
-func update_enemy_position(position):
-	
-	enemy_position = position
-
-
 func _switch_mode(_name):
 	
 	undertale_mode = !undertale_mode
 
+
+func attack_hit(body):
+
+	if "Guts" in body.name or "Wall" in body.name:
+		return
+		
+	var push_back = (body.position-position).normalized() * push_back_multiplier
+	body.got_shot(damage, push_back)
+	
+	
+func overhead():
+	var bodies = $OverHeadArea2D.get_overlapping_bodies()
+	
+	for body in bodies:
+		
+		if "Wall" in body.name:
+			body.cracked()
+		if "Guts" in body.name:
+			return
+			
+		var push_back = (body.position-position).normalized() * push_back_multiplier
+		body.got_shot(damage*2, push_back, false , true)
+
 func attack_finished(anim_name):
 	attacking = false
+
+func delete_guts():
+	queue_free()
